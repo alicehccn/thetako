84d96902f6695f1701001984b879dde956526ce6
Alice Huang <alicehccn@gmail.com>
2024-07-17 22:52:20 -0700

    render squares correctly;

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -7,23 +7,15 @@ type Props = {
   onClick: (i: number) => void;
 };
 
-const squares = Array.from(Array(9).keys());
+const squares = Array.from(Array(81).keys());
 const Board: React.FC<Props> = props => {
   const renderSquare = (i: number) => (
     <Square value={props.squares[i]} onClick={() => props.onClick(i)} />
   );
 
-  const renderRow = (i: number) => {
-    return (
-      <div className="board-row">
-        {squares.map((row, j) => renderSquare(i*j))}
-      </div>
-    )
-  }
-  
   return (
     <div>
-      {squares.map((row, i) => renderRow(i))}
+      {squares.map((row, i) => renderSquare(i))}
     </div>
   );
 };
