a089d607d68194ed7048f543285847722c2c6226
Alice Huang <alicehccn@gmail.com>
2025-02-14 14:44:30 -0800

    save

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,20 +1,51 @@
+import React from 'react';
 import '../App.css';
-
+import Modal from 'react-modal';
 
 const Content: React.FC = () => {
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
   
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <div className="App-content">
-      
-      <section>
-        Experience
+      <section className="row">
+          With a decade of experience in commercial software development from startups to mature enterprises, Alice spent the last few years building HIPAA-compliant systems for health care programs. 
       </section>
-      <section>
-        Projects
+      <section className="columns">
+            <div className="column">
+              Backend: NodeJS, Typescript, Python, Golang, Rust, Postgres, MongoDB, Postgres, BullMQ, Redis, Protobuf, gRPC, REST, Kubernetes, AWS, Heroku
+            </div>
+            <div className="column">
+              Frontend: Javascript, Typescript, React, Redux, NextJS, Webpack, HTML, CSS/SASS
+            </div>
+            <div className="column">
+              Distributed systems, cloud technologies, web technologies, API design principles, microservices, Access Control, OOP, Agile, CD/CI, APM and TDD
+            </div>
       </section>
-      <section>
-        Community
+      <section className="row">
       </section>
+          {/* <a className='collapse' onClick={openModal}>Show more</a> */}
     </div>
   );
 }
