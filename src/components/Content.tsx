640b4925b45d7af2bf2d040c87177ae10369e4c4
Alice Huang <alicehccn@gmail.com>
2025-02-17 16:47:02 -0800

    blank

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -6,9 +6,6 @@ const Content: React.FC = () => {
   return (
     <div className="App-content">
       <section className="left">
-       <div className="profile-frame">
-          <img src="./avatar.jpeg" width="100px" alt="avatar"/>
-       </div>
       </section>
       <section className="right">
       </section>
