97a2781b58313b438aebcd1f6eb3a66a27a17ed4
Alice Huang <alicehccn@gmail.com>
2025-02-18 12:20:20 -0800

    add content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -10,9 +10,33 @@ const Content: React.FC = () => {
         <img src="./email.png" alt="social" />
       </section>
       <section className="right">
-        <div>Website</div>
-        <div>e-Commerce</div>
-        <div>Cloud</div>
+        <div>
+          <h2>Web / Mobile</h2>
+          <p>Typescript</p>
+          <p>React</p>
+          <p>HTML / CSS</p>
+          <p>Webpack</p>
+          <p>Express</p>
+          <p>Expo</p>
+        </div>
+        <div>
+          <h2>e-Commerce</h2>
+          <p>UI/UX</p>
+          <p>Billing</p>
+          <p>Payment</p>
+          <p>Access Control</p>
+          <p>Maps</p>
+          <p>Analytics</p>
+        </div>
+        <div>
+          <h2>Cloud</h2>
+          <p>AWS</p>
+          <p>Heroku</p>
+          <p>Vercel</p>
+          <p>Docker</p>
+          <p>Database</p>
+          <p>CI/CD</p>
+          </div>
         </section>
     </div>
   );
