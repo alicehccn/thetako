4d4591ecf253a471a26af229fe0581267b33d400
Alice Huang <alicehccn@gmail.com>
2025-02-18 13:55:01 -0800

    divide right pannel

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -10,33 +10,40 @@ const Content: React.FC = () => {
         <img src="./email.png" alt="social" />
       </section>
       <section className="right">
-        <div>
-          <h2>Web / Mobile</h2>
-          <p>Typescript</p>
-          <p>React</p>
-          <p>HTML / CSS</p>
-          <p>Webpack</p>
-          <p>Express</p>
-          <p>Expo</p>
+        <div className="horizontal">
+          <h2>Engineering</h2>
         </div>
-        <div>
-          <h2>e-Commerce</h2>
-          <p>UI/UX</p>
-          <p>Billing</p>
-          <p>Payment</p>
-          <p>Access Control</p>
-          <p>Maps</p>
-          <p>Analytics</p>
-        </div>
-        <div>
-          <h2>Cloud</h2>
-          <p>AWS</p>
-          <p>Heroku</p>
-          <p>Vercel</p>
-          <p>Docker</p>
-          <p>Database</p>
-          <p>CI/CD</p>
+        <div className="vertical">
+          <div>
+            <h2>Web / Mobile</h2>
+            <p>Typescript</p>
+            <p>React</p>
+            <p>HTML / CSS</p>
+            <p>Webpack</p>
+            <p>Express</p>
+            <p>Expo</p>
           </div>
+          <div>
+            <h2>e-Commerce</h2>
+            <p>UI/UX</p>
+            <p>Billing</p>
+            <p>Payment</p>
+            <p>Access Control</p>
+            <p>Maps</p>
+            <p>Analytics</p>
+          </div>
+          <div>
+            <h2>Cloud</h2>
+            <p>AWS</p>
+            <p>Heroku</p>
+            <p>Vercel</p>
+            <p>Docker</p>
+            <p>Database</p>
+            <p>CI/CD</p>
+          </div>
+        </div>
+
+        
         </section>
     </div>
   );
