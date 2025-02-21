cf4250148edcd29facd745b59d78f689d8a43f9f
Alice Huang <alicehccn@gmail.com>
2025-02-21 15:24:54 -0800

    apod

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,9 +1,35 @@
 import React from "react";
 import "../App.css";
+import Modal from "react-modal";
+
+interface APOD {
+  date: string;
+  explanation: string;
+  hdurl: string;
+  media_type: string;
+  service_version: string;
+  title: string;
+  url: string;
+}
 
 const Content: React.FC = () => {
   const [showContent, setShowContent] = React.useState(false);
+  const [modalIsOpen, setIsOpen] = React.useState(false);
   const [events, setEvents] = React.useState();
+  const [image, setImage] = React.useState<APOD>();
+  const customStyles = {
+    content: {
+      width: "70%",
+      maxWidth: "800px",
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+    },
+  };
+
   React.useEffect(() => {
     if (!events) {
       fetch("https://eonet.gsfc.nasa.gov/api/v2.1/events?limit=10&status=open")
@@ -12,9 +38,42 @@ const Content: React.FC = () => {
         .catch((error) => console.error(error));
     }
   }, [events]);
+
+  React.useEffect(() => {
+    if (!image) {
+      fetch(
+        "https://api.nasa.gov/planetary/apod?api_key=ibUVEf1jTwiXdSMK0eTmaUCKi9LAIdsTAkLeiRO4"
+      )
+        .then((response) => response?.json().then((json) => setImage(json)))
+        .catch((error) => console.error(error));
+    }
+  });
+  function closeModal() {
+    setIsOpen(false);
+  }
+  function openModal() {
+    setIsOpen(true);
+  }
+
   return (
     <div className="App-content">
-      {/* <div className="apod"><img src={imageUrl}/></div> */}
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+        ariaHideApp={false}
+      >
+        <div className="apod">
+          <h2>Astronomy Picture of the Day {image?.date}</h2>
+          <img src={image?.url} />
+          <h3>{image?.title}</h3>
+          <p>{image?.explanation}</p>
+          <div className="close-btn">
+            <button onClick={closeModal}>Close</button>
+          </div>
+        </div>
+      </Modal>
       <section className="right">
         <div className={`vertical ${showContent ? "" : "hidden"}`}>
           <div>
@@ -51,8 +110,8 @@ const Content: React.FC = () => {
         >
           <h2>Engineering</h2>
         </div>
-        <div className="horizontal">
-          <h2>Data Visualization</h2>
+        <div className="horizontal" onClick={() => setIsOpen(true)}>
+          <h2>NASA</h2>
         </div>
       </section>
     </div>
