989d9c44ae197ba150bd51b0e1e7f04d4e059a24
Alice Huang <alicehccn@gmail.com>
2025-02-23 22:30:12 -0800

    Leave vertical section visible

---
+++ README.md
@@ -0,0 +1,4 @@
+https://api.nasa.gov/EPIC/archive/natural/2025/02/22/png/epic_1b_20250222004554.png?api_key=DEMO_KEY
+
+https://api.nasa.gov/
+https://epic.gsfc.nasa.gov/about/api
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "../App.css";
-import Modal from "react-modal";
-import { subDays, format } from "date-fns";
+import { Apod } from "./Apod";
 
 interface APOD {
   date: string;
@@ -18,24 +17,6 @@ const Content: React.FC = () => {
   const [showContent, setShowContent] = React.useState(false);
   const [modalIsOpen, setIsOpen] = React.useState(false);
   const [events, setEvents] = React.useState();
-  const [images, setImages] = React.useState<APOD[]>();
-  const [image, setImage] = React.useState<APOD>();
-  let [imageIndex, setImageIndex] = React.useState(1);
-
-  const apodEndDate = new Date();
-  const apodStartDate = format(subDays(apodEndDate, 7), "yyyy-MM-dd");
-  const customStyles = {
-    content: {
-      maxWidth: "800px",
-      top: "50%",
-      left: "50%",
-      right: "auto",
-      bottom: "auto",
-      marginRight: "-50%",
-      transform: "translate(-50%, -50%)",
-    },
-  };
-
   React.useEffect(() => {
     if (!events) {
       fetch("https://eonet.gsfc.nasa.gov/api/v2.1/events?limit=10&status=open")
@@ -45,22 +26,6 @@ const Content: React.FC = () => {
     }
   }, [events]);
 
-  React.useEffect(() => {
-    if (!images) {
-      fetch(
-        `https://api.nasa.gov/planetary/apod?api_key=ibUVEf1jTwiXdSMK0eTmaUCKi9LAIdsTAkLeiRO4&start_date=${apodStartDate}`
-      )
-        .then((response) =>
-          response?.json().then((json) => {
-            setImages(json);
-            setImage(json[json.length - imageIndex]);
-            setImageIndex(imageIndex);
-          })
-        )
-        .catch((error) => console.error(error));
-    }
-  });
-
   function openModal() {
     setIsOpen(true);
   }
@@ -69,58 +34,13 @@ const Content: React.FC = () => {
     setIsOpen(false);
   }
 
-  function goPrev() {
-    if (imageIndex < (images?.length ?? 0)) {
-      imageIndex++;
-      setImageIndex(imageIndex);
-      setImage(images?.[images.length - imageIndex]);
-    }
-  }
-  function goNext() {
-    if (imageIndex > 1) {
-      imageIndex--;
-      setImageIndex(imageIndex);
-      setImage(images?.[images.length - imageIndex]);
-    }
-  }
-
   return (
     <div className="App-content">
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        style={customStyles}
-        contentLabel="Example Modal"
-        ariaHideApp={false}
-      >
-        <div className="apod">
-          <h2>Astronomy Picture of the Day</h2>
-          <div className="apod-nav">
-            <div onClick={goPrev} className="prev">
-              {"<<"}
-            </div>
-            <div onClick={goNext} className="next">
-              {">>"}
-            </div>
-          </div>
-          {image?.date && <h3>{format(image.date, "MMMM dd, yyyy")}</h3>}
-          {image?.media_type === "image" && <img src={image?.url} />}
-          {image?.media_type === "video" && <iframe src={image?.url} />}
-          <h2>{image?.title}</h2>
-          <p>{image?.explanation}</p>
-          <p>&copy; {image?.copyright ? image?.copyright : "NASA"}</p>
-          <div className="close-btn">
-            <a target="_blank" href="https://apod.nasa.gov/apod/astropix.html">
-              Source
-            </a>
-            <button onClick={closeModal}>Close</button>
-          </div>
-        </div>
-      </Modal>
+      <Apod modalIsOpen={modalIsOpen} closeModal={closeModal} />
       <section className="right">
-        <div className={`vertical ${showContent ? "" : "hidden"}`}>
+        <div className={`vertical`}>
           <div>
-            <h3>Web / Mobile</h3>
+            <h2>Web / Mobile</h2>
             <p>Typescript</p>
             <p>React</p>
             <p>HTML / CSS</p>
@@ -129,7 +49,7 @@ const Content: React.FC = () => {
             <p>Expo</p>
           </div>
           <div>
-            <h3>e-Commerce</h3>
+            <h2>e-Commerce</h2>
             <p>UI/UX</p>
             <p>Billing</p>
             <p>Payment</p>
@@ -138,7 +58,7 @@ const Content: React.FC = () => {
             <p>Analytics</p>
           </div>
           <div>
-            <h3>Cloud</h3>
+            <h2>Cloud</h2>
             <p>AWS</p>
             <p>Heroku</p>
             <p>Vercel</p>
@@ -147,14 +67,9 @@ const Content: React.FC = () => {
             <p>CI/CD</p>
           </div>
         </div>
-        <div
-          className="horizontal"
-          onClick={() => setShowContent(!showContent)}
-        >
-          <h2>Engineering</h2>
-        </div>
+
         <div className="horizontal" onClick={openModal}>
-          <h2>APOD</h2>
+          <h2>NASA</h2>
         </div>
       </section>
     </div>
