0ec01db61b1cb724f859843809eddf3d6a944005
Alice Huang <alicehccn@gmail.com>
2025-02-26 19:53:35 -0800

    render event summary

--- src/components/Eonet.tsx
+++ src/components/Eonet.tsx
@@ -4,13 +4,12 @@ import { FETCH_EVENT_URL, getDateString } from "../constant";
 import { Autocomplete, Button, TextField } from "@mui/material";
 
 interface EventResponse {
-  id: string;
   title: string;
   description: string;
   link: string;
   categories: {
     id: string;
-    url: string;
+    title: string;
   }[];
   sources: {
     id: string;
@@ -30,7 +29,7 @@ type ModalProps = {
 
 const MODAL_STYLES = {
   content: {
-    width: "900px",
+    width: "800px",
     maxHeight: "95vh",
     top: "50%",
     left: "50%",
@@ -43,6 +42,7 @@ const MODAL_STYLES = {
 
 export const EONET: React.FC<ModalProps> = ({ modalIsOpen, closeModal }) => {
   const [events, setEvents] = React.useState<EventResponse[]>();
+  const [event, setEvent] = React.useState<EventResponse>();
   React.useEffect(() => {
     if (!events) {
       fetch(FETCH_EVENT_URL)
@@ -56,6 +56,32 @@ export const EONET: React.FC<ModalProps> = ({ modalIsOpen, closeModal }) => {
     return;
   }
 
+  function renderSummary(event: EventResponse) {
+    return (
+      <div className="event">
+        <div>
+          Categories:{" "}
+          {event.categories.map((c, i) => (
+            <span key={i}>{c.title}</span>
+          ))}
+        </div>
+        <div>
+          Geometries:{" "}
+          {event.geometries.map((c, i) => (
+            <span key={i}>[{c.coordinates}], </span>
+          ))}
+        </div>
+        <div>
+          Source:{" "}
+          {event.sources.map((c, i) => (
+            <span key={i}>{c.url}</span>
+          ))}
+        </div>
+        <div>Link: {event.link}</div>
+        {event.description && <div>Desrciption: {event.description}</div>}
+      </div>
+    );
+  }
   return (
     <ReactModal
       isOpen={modalIsOpen}
@@ -67,15 +93,28 @@ export const EONET: React.FC<ModalProps> = ({ modalIsOpen, closeModal }) => {
       <div className="eonet">
         <h2>Earth Observatory Natural Event Tracker</h2>
         <Autocomplete
+          value={
+            event
+              ? {
+                  label: `${getDateString(event?.geometries[0].date)} | ${event?.title}`,
+                  option: event,
+                }
+              : null
+          }
           options={events.map((event) => {
             return {
               label: `${getDateString(event.geometries[0].date)} | ${event.title}`,
-              event,
+              option: event,
             };
           })}
+          onChange={(e, value) => {
+            e.preventDefault();
+            setEvent(value?.option);
+          }}
           sx={{ width: "90%", margin: "auto" }}
           renderInput={(params) => <TextField {...params} label="Event" />}
         />
+        {event && renderSummary(event)}
       </div>
     </ReactModal>
   );
