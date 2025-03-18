e043d56c48e3b3bbc9c48b1d6d8f9f9b995774bb
Alice Huang <alicehccn@gmail.com>
2025-03-17 19:40:57 -0700

    set min and max date

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -2,6 +2,8 @@ import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import dayjs from "dayjs";
+import { getApiDate } from "../constant";
 
 type DatePickerProps = {
   setValue: (props: any) => void;
@@ -10,7 +12,11 @@ const BasicDatePicker: React.FC<DatePickerProps> = ({ setValue }) => {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DemoContainer components={["DatePicker"]}>
-        <DatePicker onChange={setValue} />
+        <DatePicker
+          onChange={setValue}
+          minDate={dayjs(getApiDate(365))}
+          maxDate={dayjs(getApiDate(1))}
+        />
       </DemoContainer>
     </LocalizationProvider>
   );
