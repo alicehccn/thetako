877c56be155d6944d7ca66988844e662b985a1b0
Alice Huang <alicehccn@gmail.com>
2025-04-07 14:17:15 -0700

    save main layer as const

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import ReactModal from "react-modal";
-import { fetchWeatherApi, formatDateTime } from "../constant";
+import { fetchWeatherApi, formatDateTime, MAIN_LAYER } from "../constant";
 import Switch from "@mui/material/Switch";
 import ReplayIcon from "@mui/icons-material/Replay";
 import mapboxgl, { Point } from "mapbox-gl";
@@ -40,16 +40,18 @@ const MODAL_STYLES = {
   },
 };
 
-type Alerts = {
+type AlertGroup = {
   features: Feature[];
   updated: string;
   type: "FeatureCollection";
 };
 
 export const Weather: React.FC<ModalProps> = ({ modalIsOpen, closeModal }) => {
-  const [alerts, setAlerts] = useState<Alerts>();
+  const [alerts, setAlerts] = useState<AlertGroup>();
   const [reloading, setReload] = useState(false);
   const [darkMode, setDarkMode] = useState<boolean>(false);
+  const label = { inputProps: { "aria-label": "Dark Mode" } };
+  const [alertGroup, setAlertGroup] = useState<Feature[]>();
   useEffect(() => {
     if (!alerts || !!reloading) {
       fetch(fetchWeatherApi())
@@ -64,12 +66,11 @@ export const Weather: React.FC<ModalProps> = ({ modalIsOpen, closeModal }) => {
   if (!alerts) {
     return;
   }
-  const result = _.chain(alerts.features)
+  const alertGroups = _.chain(alerts.features)
     .groupBy("properties.event")
     .map((value, key) => ({ label: key, value }))
     .filter((value) => value.label !== "Test Message")
     .value();
-  const label = { inputProps: { "aria-label": "Dark Mode" } };
   return (
     <ReactModal
       isOpen={modalIsOpen}
@@ -89,7 +90,7 @@ export const Weather: React.FC<ModalProps> = ({ modalIsOpen, closeModal }) => {
             >
               Current Alerts: {alerts.features.length}
             </Box>
-            {result.splice(0, 3).map((event) => (
+            {alertGroups.splice(0, 3).map((event) => (
               <Box
                 key={event.label}
                 fontSize={16}
@@ -103,7 +104,7 @@ export const Weather: React.FC<ModalProps> = ({ modalIsOpen, closeModal }) => {
           <AccordionDetails>
             <Autocomplete
               disablePortal
-              options={result}
+              options={alertGroups}
               sx={{ width: "100%", margin: "auto" }}
               renderInput={(params) => (
                 <TextField {...params} label="Active Alerts" />
@@ -111,10 +112,13 @@ export const Weather: React.FC<ModalProps> = ({ modalIsOpen, closeModal }) => {
               getOptionLabel={(option) =>
                 `${option.label}: ${option.value.length}`
               }
+              onChange={(e, option, i, d) => {
+                setAlertGroup(option?.value);
+              }}
             />
           </AccordionDetails>
         </Accordion>
-        <Mapbox data={alerts} darkMode={darkMode} />
+        <Mapbox data={alerts} darkMode={darkMode} alertGroup={alertGroup} />
         <Box textAlign="center">
           <IconButton
             aria-label="Refresh"
@@ -145,9 +149,10 @@ export const Weather: React.FC<ModalProps> = ({ modalIsOpen, closeModal }) => {
 type MapProps = {
   data: GeoJSON;
   darkMode: boolean;
+  alertGroup?: Feature[];
 };
 
-const Mapbox: React.FC<MapProps> = ({ data, darkMode }) => {
+const Mapbox: React.FC<MapProps> = ({ data, darkMode, alertGroup }) => {
   const mapContainerRef = useRef<any>();
   const mapRef = useRef<any>();
   const [selectedFeature, setSelectedFeature] = useState<Feature | null>();
@@ -177,122 +182,7 @@ const Mapbox: React.FC<MapProps> = ({ data, darkMode }) => {
         data,
       });
 
-      map.addLayer(
-        {
-          id: "alerts-heat",
-          type: "heatmap",
-          source: "alerts",
-          maxzoom: 9,
-          paint: {
-            // Increase the heatmap weight based on frequency and property magnitude
-            "heatmap-weight": [
-              "interpolate",
-              ["linear"],
-              ["get", "mag"],
-              0,
-              0,
-              6,
-              1,
-            ],
-            // Increase the heatmap color weight weight by zoom level
-            // heatmap-intensity is a multiplier on top of heatmap-weight
-            "heatmap-intensity": [
-              "interpolate",
-              ["linear"],
-              ["zoom"],
-              0,
-              1,
-              9,
-              3,
-            ],
-            // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
-            // Begin color ramp at 0-stop with a 0-transparancy color
-            // to create a blur-like effect.
-            "heatmap-color": [
-              "interpolate",
-              ["linear"],
-              ["heatmap-density"],
-              0,
-              "rgba(33,102,172,0)",
-              0.2,
-              "rgb(103,169,207)",
-              0.4,
-              "rgb(209,229,240)",
-              0.6,
-              "rgb(253,219,199)",
-              0.8,
-              "rgb(239,138,98)",
-              1,
-              "rgb(178,24,43)",
-            ],
-            // Adjust the heatmap radius by zoom level
-            "heatmap-radius": [
-              "interpolate",
-              ["linear"],
-              ["zoom"],
-              0,
-              2,
-              9,
-              20,
-            ],
-            // Transition from heatmap to circle layer by zoom level
-            "heatmap-opacity": [
-              "interpolate",
-              ["linear"],
-              ["zoom"],
-              7,
-              1,
-              9,
-              0,
-            ],
-          },
-        },
-        "waterway-label",
-      );
-
-      map.addLayer(
-        {
-          id: "alerts-point",
-          type: "circle",
-          source: "alerts",
-          minzoom: 7,
-          paint: {
-            // Size circle radius by alert magnitude and zoom level
-            "circle-radius": [
-              "interpolate",
-              ["linear"],
-              ["zoom"],
-              7,
-              ["interpolate", ["linear"], ["get", "mag"], 1, 1, 6, 4],
-              16,
-              ["interpolate", ["linear"], ["get", "mag"], 1, 5, 6, 50],
-            ],
-            // Color circle by alert magnitude
-            "circle-color": [
-              "interpolate",
-              ["linear"],
-              ["get", "mag"],
-              1,
-              "rgba(33,102,172,0)",
-              2,
-              "rgb(103,169,207)",
-              3,
-              "rgb(209,229,240)",
-              4,
-              "rgb(253,219,199)",
-              5,
-              "rgb(239,138,98)",
-              6,
-              "rgb(178,24,43)",
-            ],
-            "circle-stroke-color": "white",
-            "circle-stroke-width": 1,
-            // Transition from heatmap to circle layer by zoom level
-            "circle-opacity": ["interpolate", ["linear"], ["zoom"], 7, 0, 8, 1],
-          },
-        },
-        "waterway-label",
-      );
+      map.addLayer(MAIN_LAYER("alerts"), "waterway-label");
       map.on("click", "alerts-heat", (e: any) => {
         setSelectedFeature(e.features[0]);
         setPoint(e.point);
--- src/constant.ts
+++ src/constant.ts
@@ -1,4 +1,5 @@
 import { format, subDays } from "date-fns";
+import { Layer } from "mapbox-gl";
 
 export const APOD_HOMEPAGE = "https://apod.nasa.gov/apod/astropix.html";
 
@@ -111,3 +112,43 @@ export const PIE_CHART = [
   "#6e6e6e",
   "#666666",
 ];
+
+export const MAIN_LAYER = (source: string): Layer => {
+  return {
+    id: "alerts-heat",
+    type: "heatmap",
+    source,
+    maxzoom: 9,
+    paint: {
+      // Increase the heatmap weight based on frequency and property magnitude
+      "heatmap-weight": ["interpolate", ["linear"], ["get", "mag"], 0, 0, 6, 1],
+      // Increase the heatmap color weight weight by zoom level
+      // heatmap-intensity is a multiplier on top of heatmap-weight
+      "heatmap-intensity": ["interpolate", ["linear"], ["zoom"], 0, 1, 9, 3],
+      // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
+      // Begin color ramp at 0-stop with a 0-transparancy color
+      // to create a blur-like effect.
+      "heatmap-color": [
+        "interpolate",
+        ["linear"],
+        ["heatmap-density"],
+        0,
+        "rgba(33,102,172,0)",
+        0.2,
+        "rgb(103,169,207)",
+        0.4,
+        "rgb(209,229,240)",
+        0.6,
+        "rgb(253,219,199)",
+        0.8,
+        "rgb(239,138,98)",
+        1,
+        "rgb(178,24,43)",
+      ],
+      // Adjust the heatmap radius by zoom level
+      "heatmap-radius": ["interpolate", ["linear"], ["zoom"], 0, 2, 9, 20],
+      // Transition from heatmap to circle layer by zoom level
+      "heatmap-opacity": ["interpolate", ["linear"], ["zoom"], 7, 1, 9, 0],
+    },
+  };
+};
