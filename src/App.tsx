a7ffa3dba3ac5446279d153b9dfbbb961e808c54
Alice Huang <alicehccn@gmail.com>
2025-04-29 13:47:18 -0700

    restructure files

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import Home from "./components/Home";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Header />
       <Home />
-      <Footer />
     </div>
   );
 };
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -10,25 +10,49 @@ const Content: React.FC = () => {
   const [selectedIndex, setSelectedIndex] = useState(0);
 
   return (
-    <div className="App-content">
-      <APOD
-        modalIsOpen={selectedIndex === 1}
-        closeModal={() => setSelectedIndex(0)}
-      />
-      <Epic
-        modalIsOpen={selectedIndex === 2}
-        closeModal={() => setSelectedIndex(0)}
-      />
-      <Weather
-        modalIsOpen={selectedIndex === 3}
-        closeModal={() => setSelectedIndex(0)}
-      />
-      <MPieChart />
-      <SplitButton
-        setSelectedIndex={setSelectedIndex}
-        selectedIndex={selectedIndex}
-      />
-    </div>
+    <>
+      <div className="App-header">
+        <div className="social">
+          <a href="mailto:alicehccn@gmail.com">
+            <img src="./email.png" />
+            Email
+          </a>
+          <a target="_blank" href="https://www.linkedin.com/in/alicehccn/">
+            <img src="./linkedin.png" />
+            LinkedIn
+          </a>
+          <a target="_blank" href="https://www.github.com/alicehccn/">
+            <img src="./github.png" />
+            Github
+          </a>
+        </div>
+        <div className="logo">
+          <img src="./logo2.png" alt="logo" />
+        </div>
+      </div>
+      <div className="App-content">
+        <APOD
+          modalIsOpen={selectedIndex === 1}
+          closeModal={() => setSelectedIndex(0)}
+        />
+        <Epic
+          modalIsOpen={selectedIndex === 2}
+          closeModal={() => setSelectedIndex(0)}
+        />
+        <Weather
+          modalIsOpen={selectedIndex === 3}
+          closeModal={() => setSelectedIndex(0)}
+        />
+        <MPieChart />
+        <SplitButton
+          setSelectedIndex={setSelectedIndex}
+          selectedIndex={selectedIndex}
+        />
+      </div>
+      <footer>
+        <span>&copy; 2025-2026 Alice Huang</span>
+      </footer>
+    </>
   );
 };
 export default Content;
